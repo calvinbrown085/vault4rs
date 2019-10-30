c18faab0526b37a7c84fbae69696904b75777a84
Calvin Brown <calvinb085@icloud.com>
2019-10-30 08:09:56 -0500

    Adding read_secret function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+use reqwest::{Client, RequestBuilder, Url};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct VaultSecret<T> {
+    data: T,
+    lease_duration: i64,
+    lease_id: String,
+    renewable: bool,
+}
+
+fn read_secret(client: Client, mut url: Url, token: &str, secret_path: &str) -> VaultSecret<String> {
+    url.set_path(&format!("/v1/{}", secret_path));
+    let request: RequestBuilder = client.get(url).header("X-Vault-Token", token);
+    let resp: String = request.send().unwrap().json().unwrap();
+
+    let a: VaultSecret<String> = serde_json::from_str(&resp).unwrap();
+    a
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
